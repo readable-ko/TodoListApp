2997362fb2ba18a24d9d19b65f27783f1f4a45c1
readable-ko <21700026@handong.edu>
2021-09-22 13:31:43 +0900

    updated new

--- src/com/todo/service/TodoUtil.java
+++ src/com/todo/service/TodoUtil.java
@@ -1,5 +1,11 @@
 package com.todo.service;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.*;
 
 import com.todo.dao.TodoItem;
@@ -12,21 +18,23 @@ public class TodoUtil {
 		String title, desc;
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("\n"
-				+ "========== Create item Section\n"
-				+ "enter the title\n");
+		System.out.println("[항목 추가] \n"
+				+ "제목 > ");
 		
 		title = sc.next();
 		if (list.isDuplicate(title)) {
-			System.out.printf("title can't be duplicate");
+			System.out.printf("제목이 중복됩니다!");
+			sc.close();
 			return;
 		}
 		
-		System.out.println("enter the description");
-		desc = sc.next();
+		System.out.println("내용 > ");
+		desc = sc.nextLine().trim();
 		
 		TodoItem t = new TodoItem(title, desc);
 		list.addItem(t);
+		System.out.println("내용이 추가되었습니다.");
+		sc.close();
 	}
 
 	public static void deleteItem(TodoList l) {
@@ -34,17 +42,17 @@ public class TodoUtil {
 		Scanner sc = new Scanner(System.in);
 		String title = sc.next();
 		
-		System.out.println("\n"
-				+ "========== Delete Item Section\n"
-				+ "enter the title of item to remove\n"
-				+ "\n");
+		System.out.println("[항목 삭제]\n"
+				+ "삭제할 항목의 제목을 입력하시오 > ");
 		
 		for (TodoItem item : l.getList()) {
 			if (title.equals(item.getTitle())) {
 				l.deleteItem(item);
+				System.out.println("삭제되었습니다.");
 				break;
 			}
 		}
+		sc.close();
 	}
 
 
@@ -52,39 +60,78 @@ public class TodoUtil {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("\n"
-				+ "========== Edit Item Section\n"
-				+ "enter the title of the item you want to update\n"
-				+ "\n");
-		String title = sc.next().trim();
+		System.out.println("[항목 수정]\n"
+				+ "수정할 항목의 제목을 입력하시오 > ");
+		
+		String title = sc.nextLine().trim();
 		if (!l.isDuplicate(title)) {
-			System.out.println("title doesn't exist");
+			System.out.println("존재하지 않는 제목입니다!");
+			sc.close();
 			return;
 		}
 
-		System.out.println("enter the new title of the item");
-		String new_title = sc.next().trim();
+		System.out.println("새 제목 > ");
+		String new_title = sc.nextLine().trim();
 		if (l.isDuplicate(new_title)) {
-			System.out.println("title can't be duplicate");
+			System.out.println("중복된 제목입니다!");
+			sc.close();
 			return;
 		}
 		
-		System.out.println("enter the new description ");
-		String new_description = sc.next().trim();
+		System.out.println("새 내용 > ");
+		String new_description = sc.nextLine().trim();
 		for (TodoItem item : l.getList()) {
 			if (item.getTitle().equals(title)) {
 				l.deleteItem(item);
 				TodoItem t = new TodoItem(new_title, new_description);
 				l.addItem(t);
-				System.out.println("item updated");
+				System.out.println("수정되었습니다.");
 			}
 		}
-
+		sc.close();
 	}
 
 	public static void listAll(TodoList l) {
+		System.out.println("[전체 목록]");
 		for (TodoItem item : l.getList()) {
-			System.out.println("Item Title: " + item.getTitle() + "  Item Description:  " + item.getDesc());
+			System.out.println(item.toString());
+		}
+	}
+	
+	public static void saveList(TodoList l, String filename) {
+		try {
+			Writer w = new FileWriter(filename);
+			w.write(l.toString());
+			System.out.println("데이터 저장 완료!!");
+			w.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void loadList(TodoList l, String filename) {
+		// TODO Auto-generated method stub
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(filename));
+			
+			String temp;
+			while((temp = br.readLine()) != null) {
+				StringTokenizer st = new StringTokenizer(temp,"##");
+				String title = st.nextToken();
+				String desc = st.nextToken();
+				String date = st.nextToken();
+				
+				TodoItem item = new TodoItem(title,desc);
+				item.setCurrent_date(date);
+			}
+			br.close();
+			System.out.println("데이터 로딩 완료!");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
